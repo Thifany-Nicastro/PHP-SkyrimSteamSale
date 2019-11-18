3b656aaa81133dec85f2a15c7e44640d46c51b72
Thifany Nicastro <thifanynicastro@hotmail.com>
2019-11-18 09:18:00 -0300

    Create lang.en.php

---
+++ lang.en.php
@@ -0,0 +1,15 @@
+<?php
+/* 
+------------------
+Language: English
+------------------
+*/
+
+$lang = array();
+
+$lang['PAGE_TITLE'] = 'ISOST';
+$lang['MAIN_TITLE'] = 'Is Skyrim on sale today?';
+$lang['RES_POSITIVE'] = 'Yes!';
+$lang['RES_NEGATIVE'] = 'No.';
+$lang['BUTTON_STEAM'] = 'GO!';
+?>
