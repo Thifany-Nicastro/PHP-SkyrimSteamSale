c35fd930e601e77c481f0c9b99431a6dc54647cd
Thifany Nicastro <thifanynicastro@hotmail.com>
2019-11-18 09:47:55 -0300

    Update common.php

--- common.php
+++ common.php
@@ -1,27 +1,44 @@
 <?php
+    
+    /* 
+    ------------------
+    STEAM API
+    ------------------
+    */
+    
+    $appid = "489830";
+    $api_url = "https://store.steampowered.com/api/appdetails?appids=$appid&cc=br&l=pt";
+    $json = json_decode(file_get_contents($api_url), true);
 
-if(isSet($_GET['lang']))
-{
-    $lang = $_GET['lang'];
-}
-else
-{
-    $lang = 'en';
-}
 
-switch ($lang) {
-  case 'en':
-  $lang_file = 'lang.en.php';
-  break;
-
-  case 'pt':
-  $lang_file = 'lang.pt.php';
-  break;
-
-  default:
-  $lang_file = 'lang.en.php';
-
-}
-
-include_once 'languages/'.$lang_file;
+    /* 
+    ------------------
+    LANGUAGE
+    ------------------
+    */
+    
+    if(isset($_GET['lang']))
+    {
+        $lang = $_GET['lang'];
+    }
+    else
+    {
+        $lang = 'en';
+    }
+    
+    switch ($lang) 
+    {
+      case 'en':
+      $lang_file = 'lang.en.php';
+      break;
+      
+      case 'pt':
+      $lang_file = 'lang.pt.php';
+      break;
+      
+      default:
+      $lang_file = 'lang.en.php';
+    }
+    
+    include_once 'languages/'.$lang_file;
 ?>
